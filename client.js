ac166a30ce73e2c673cbcc2a36b93fd7642da74b
t5krishn <tk95@live.ca>
2019-07-03 17:15:50 +0000

    Compartmentalized connect function into client.js

---
+++ client.js
@@ -0,0 +1,22 @@
+/**
+ * Establishes connection with the game server
+ */
+
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  return conn;
+}
+
+module.exports = {connect};
