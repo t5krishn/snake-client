10e77b1e1f61bd02c7525350f1bdfbcda174f20b
t5krishn <tk95@live.ca>
2019-07-03 19:32:16 +0000

    Stdin callback for data event

--- play.js
+++ play.js
@@ -1,8 +1,27 @@
 const connect = require('./client').connect;
-const net = require('net');
+// const net = require('net');
 
 
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = function() {
+    stdin.on('data', key => {
+      console.log(key);
+      if (key === '\u0003'){
+        process.exit();
+      }
+    })
+  }();
+  return stdin;
+}
 
 
 console.log('Connecting ...');
+
 connect();
+setupInput();
